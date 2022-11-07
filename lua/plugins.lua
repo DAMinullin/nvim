b502222dabf11492553c8f58c8c2a3acbbd61270
DAMinullin <>
2022-11-07 12:33:47 +0300

    plugins base settings

---
+++ lua/plugins.lua
@@ -0,0 +1,33 @@
+local fn = vim.fn
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+local packer_bootstrap = nil
+if fn.empty(fn.glob(install_path)) > 0 then
+	packer_botstrap = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	vim.api.nvim_command("packadd packer.nvim")
+end
+
+local on_file_open = { "BufRead", "BufWinEnter"}
+
+return require("packer").startup({
+	function(use)
+		use("wbthomason/packer.nvim")
+
+		if packer_bootstrap then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "single" })
+			end,
+		},
+	},
+})
