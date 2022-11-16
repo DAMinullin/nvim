51ccf6871d63e90d904f46bf71b99e356ea58fcb
DAMinullin <>
2022-11-16 12:53:50 +0300

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,6 @@
 require("nvim-treesitter.configs").setup{
 	ensure_installed = { "python", "lua" },
-	--ensure_installed = { "python" },
-	--ensure_installed = "all",
   sync_install = false,
-  --auto_install = true,
   --ignore_install = { "javascript" },
   highlight = {
     enable = true,
@@ -17,4 +14,16 @@ require("nvim-treesitter.configs").setup{
     --end,
     additional_vim_regex_highlighting = false,
   },
+	indent = {
+		enable = true,
+		disable = { "" },
+	},
+	rainbow = {
+    enable = true,
+    disable = { "python" },
+    --extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+    --max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    -- colors = {}, -- table of hex strings
+    -- termcolors = {} -- table of colour name strings
+  }
 }
