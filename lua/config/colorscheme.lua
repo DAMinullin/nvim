c625887ab63e97bfb81cf125198d32c5809287cf
DAMinullin <>
2022-11-17 10:32:12 +0300

    fix status checking

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,7 +1,7 @@
 local default_scheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. default_scheme)
-if not staus_ok then
+if not status_ok then
 	vim.notify("colorscheme " .. default_scheme .. " not found!")
 	return
 end
